6bb695e308ecc7c3df4024fe382ce1bef6f2f2b8
root <root@DESKTOP-UKA4A0D.localdomain>
2022-08-04 23:56:00 +0100

    lockboxes

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from typing import List
+
+
+def canUnlockAll(boxes):
+    if (len(boxes) == 0 or boxes is None):
+        return False
+    unlocked = []
+    for box in boxes[1:]:
+        for i in box:
+            if (i > (len(boxes) - 1)):
+                continue
+            elif(boxes[i] and not(i in unlocked)):
+                unlocked.append(i)
+    if ( len(boxes) - 1) == len(unlocked)):
+        return False
+    return True
