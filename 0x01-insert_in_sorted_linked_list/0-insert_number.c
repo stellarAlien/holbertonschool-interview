258d9f02a7bcbcc39a4d37e544381374bd923a32
root <root@DESKTOP-UKA4A0D.localdomain>
2022-08-10 15:37:20 +0100

    insert node

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * insert_node - insert a node in a sortedlist
+ * @head: head of list
+ * @number: data
+ * Return: node if success else NULL 
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+    listint_t *p, *node;
+
+    if(*head == NULL || number < 0)
+    {
+        return NULL;
+    }
+    node = (listint_t *)malloc(sizeof(listint_t));
+    if(node)
+    {
+        node->n = number;
+        node->next = NULL;
+    }
+    else
+    {
+        return(NULL);
+    }
+    p = *head;
+    while(p->next)
+    {
+        if(number < p->n)
+        {
+            node->next = p->next;
+            p->next = node;
+            return(node);
+        }
+        p = p->next;
+    }
+    p->next = node;
+    return(node);
+}
