9cca61c12efdefdab2d68d6e655852118e528c52
root <root@DESKTOP-UKA4A0D.localdomain>
2022-08-25 23:30:04 +0100

    minimium operations

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Minimum Operations Module
+"""
+
+
+def minOperations(n):
+    '''minimium operations of copyAll and paste to constuct H*n string'''
+    if n < 2:
+        return 0
+    div, ops_count =  2, 0
+    while n > 1:
+        if n % div == 0:
+            n = n / div
+            ops_count = ops_count +  div
+        else:
+            div += 1
+    return ops_count
+    
