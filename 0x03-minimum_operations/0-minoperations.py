62519ff50c3aff29a010316d53edf318dad967bd
root <root@DESKTOP-UKA4A0D.localdomain>
2022-08-25 23:43:02 +0100

    minimium operations

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -10,11 +10,11 @@ def minOperations(n):
     """minimium operations of copyAll and paste to constuct H*n string"""
     if n < 2:
         return 0
-    div, ops_count =  2, 0
+    div, ops_count = 2, 0
     while n > 1:
         if n % div == 0:
             n = n / div
-            ops_count = ops_count +  div
+            ops_count = ops_count + div
         else:
             div += 1
     return ops_count
