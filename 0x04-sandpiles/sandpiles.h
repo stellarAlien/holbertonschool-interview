1da144c7384b631868c66ae3fdc193f70b4aca42
root <root@DESKTOP-UKA4A0D.localdomain>
2022-09-01 23:02:40 +0100

    fixed betty style

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -1,7 +1,10 @@
-#include <stdlib.h>
-#include <stdio.h>
 #ifndef SANDPILES_H
 #define SANDPILES_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 int unsatble(int grid[3][3]);
 #endif
