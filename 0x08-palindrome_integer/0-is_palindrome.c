bc3502f90b088984a79ae3154e780fb9b1782af5
root <root@DESKTOP-UKA4A0D.localdomain>
2022-09-29 23:08:30 +0100

    clean palindrome function

---
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,23 @@
+#include "palindrome.h"
+
+/**
+ * is_palindrome - checks if int is palindrome
+ *
+ * @n: int to check
+ * Return: int
+ */
+int is_palindrome(unsigned long n)
+{
+	unsigned long remainder, reversed = 0, aux;
+
+	aux = n;
+	while (aux != 0)
+	{
+		remainder = aux % 10;
+		reversed = reversed * 10 + remainder;
+		aux /= 10;
+	}
+	if (reversed != n)
+	return (0);
+	return (1);
+}
---
+++ 0x08-palindrome_integer/README.md
@@ -0,0 +1,3 @@
+# 0x08-palindrome_integer
+Write a function that checks whether or not a given unsigned integer is a palindrome.
+
