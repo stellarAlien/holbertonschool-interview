3c8b1c50c46f83ce981142fd22496605ae640653
stellarAlien <saifgaida@gmail.com>
2022-10-06 23:44:25 +0100

    validate utf-8 by checking every byte

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+
+def validUTF8(data):
+    """
+    check if data is valid utf-8
+    """
+    n = 0
+    for i in data:
+        d = format(i, '#010b')[-8:]
+        if n == 0:
+            for b in d:
+                if b == '0':
+                    break
+                n += 1
+            if n == 0:  # type 0 is considered valid
+                continue
+            if n == 1 or n > 4:  # starting with 10 is invalid
+                return False
+        elif not d[0:1] == '10':  # check if next bytes are of type 1
+            return False
+        n = n - 1
+    if n == 0:  # if  n  next bytes are of Type 1 return True
+        return True
+    else:
+        return False
