8f666f91209050b25f54403d7ac352385e888b01
stellarAlien <saifgaida@gmail.com>
2022-10-21 14:21:45 +0100

    handling malloc return

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -15,8 +15,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	if (!node)
 	{
 		free(node);
-		return (NULL)
-
+		return (NULL);
 	}
 	node->parent = NULL;
 	node->left = NULL;
