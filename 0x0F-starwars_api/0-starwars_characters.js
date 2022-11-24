a5c73cc121038e5f68a13be4d85dc07e079ab679
stellarAlien <saifgaida@gmail.com>
2022-11-24 22:16:04 +0100

    playing with star wars api

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,19 +1,19 @@
-import request from 'request';
-import { argv } from 'process';
+#!/usr/bin/node
+const request = require('request');
+const film = process.argv[2];
 
-episode = argv(2);
-url = `https://swapi-api.hbtn.io/api/${epsiode}`;
-request(url, function (error, response, body)  {
-    if (error) throw error;
-    let json = JSON.parse(body).characters;
-    json.foreach((item, index) => {
-        request(item, (error, response, body) => {
-            if (error) throw error;
-
-            let n = JSON.parse(body).name;
-            console.log(n);
-        });
-    }       
-    );
-    
+url = 'https://swapi-api.hbtn.io/api/films/'
+request(api + film, (error, response, body) => {
+  if (error) throw error;
+  const characters = JSON.parse(body).characters;
+  viewNames(characters);
 });
+
+const viewNames = (names, i = 0) => {
+  if (i === names.length) return;
+  request(names[i], (error, response, body) => {
+    if (error) throw error;
+    console.log(JSON.parse(body).name);
+    viewNames(names, i + 1);
+  });
+};
