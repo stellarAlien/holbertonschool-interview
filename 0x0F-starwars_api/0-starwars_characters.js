d07de837bda16028384f1811231a23d6f8fe5f23
stellarAlien <saifgaida@gmail.com>
2022-11-24 22:33:23 +0100

    reformatting

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -2,19 +2,19 @@
 import request from 'request';
 const episode = process.argv[2];
 
-const print_names = (names, i = 0) => {
-  if (!names[i]) return;
-  request(names[i], (error, response, body) => {
-    if (error) throw error;
-    console.log(JSON.parse(body).name);
-    print_names(names, i + 1);
-  });
+const Print_Names = (names, i = 0) => {
+	if (!names[i]) return;
+	request(names[i], (error, response, body) => {
+		if (error) throw error;
+		console.log(JSON.parse(body).name);
+		Print_Names(names, i + 1);
+	});
 };
 
 url = `https://swapi-api.hbtn.io/api/films/${episode}`
 
 request(url, (error, response, body) => {
-  if (error) throw error;
-  const characters = JSON.parse(body).characters;
-  print_names(characters);
+	if (error) throw error;
+	const characters = JSON.parse(body).characters;
+	Print_Names(characters);
 });
