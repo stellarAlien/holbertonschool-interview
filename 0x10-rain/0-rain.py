adef22eecb889ee9d17fe31172148e42957e78fb
stellarAlien <saifgaida@gmail.com>
2022-11-29 23:22:55 +0100

    rain with two pointers

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""calculate volume of water using two pointers"""
+
+
+def rain(walls):
+    """calculate volume of water using two pointers"""
+    s, d = 0, 0
+    for i in walls:
+        if not i:
+            d += 1
+        break
+
+    for i in range(d + 2, len(walls)):
+        if walls[i]:
+            s += min(walls[d], walls[i]) * (i - d - 1)
+            d = i
+    return s
