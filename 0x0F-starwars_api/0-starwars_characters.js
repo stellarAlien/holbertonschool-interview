0866746a6d87c24cfdc68c2dc184067d1fc9bb36
stellarAlien <saifgaida@gmail.com>
2023-01-28 15:31:53 +0100

    starwars api

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,20 +1,19 @@
 #!/usr/bin/node
-import request from 'request';
-const episode = process.argv[2];
+const request = require("request");
+const movieId = process.argv[2];
 
-const Print_Names = (names, i = 0) => {
-	if (!names[i]) return;
-	request(names[i], (error, response, body) => {
-		if (error) throw error;
-		console.log(JSON.parse(body).name);
-		Print_Names(names, i + 1);
-	});
-};
-
-url = `https://swapi-api.hbtn.io/api/films/${episode}`
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
 
 request(url, (error, response, body) => {
-	if (error) throw error;
-	const characters = JSON.parse(body).characters;
-	Print_Names(characters);
+  if (!error && response.statusCode === 200) {
+    const movie = JSON.parse(body);
+    movie.characters.forEach(characterUrl => {
+      request(characterUrl, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+          const character = JSON.parse(body);
+          console.log(character.name);
+        }
+      });
+    });
+  }
 });
