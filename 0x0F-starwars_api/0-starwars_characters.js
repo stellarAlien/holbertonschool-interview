fd876e40aaadf6e38ef027f8e3163c9bd134f1c1
stellarAlien <saifgaida@gmail.com>
2023-01-28 15:35:42 +0100

    correct out^put

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,18 +1,32 @@
 #!/usr/bin/node
-const request = require("request");
-const movieId = process.argv[2];
+const request = require('request');
 
-const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+const Args = process.argv.slice(2);
+const Id = Args[0];
 
-request(url, (error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    const movie = JSON.parse(body);
-    movie.characters.forEach(characterUrl => {
-      request(characterUrl, (error, response, body) => {
-        if (!error && response.statusCode === 200) {
-          const character = JSON.parse(body);
-          console.log(character.name);
+const options = {
+  url: `https://swapi-api.hbtn.io/api/films/${Id}/`,
+  json: true,
+  transform (body, response) {
+    if (response.headers['content-type'] === 'application/json') {
+      response.body = JSON.parse(body);
+    }
+    return response;
+  }
+};
+
+request(options, async (error, response, body) => {
+  if (error) {
+    throw new Error(`HTTP error: ${response.status}`);
+  }
+  for (const characterURL of response.body.characters) {
+    await new Promise((resolve, reject) => {
+      request(characterURL, (err, res, body1) => {
+        if (err) {
+          reject(new Error(`HTTP error: ${res.status}`));
         }
+        console.log(JSON.parse(body1).name);
+        resolve();
       });
     });
   }
